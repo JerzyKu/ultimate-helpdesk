6211ee4035433548a7ee0b590b9e616ec9af701a
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-06-23 21:51:43 +0200

    create user and employee schema

---
+++ server/model/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        require: true
+    },
+    roles: {
+        User: {
+            type: Number,
+            default: 2001
+        },
+        Editor: Number,
+        Admin: Number
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    refreshToken: {
+        type: String
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('User', userSchema)
