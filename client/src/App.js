065ce8ebe7569b36dbe013b3aea534203889665b
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-06-27 16:59:27 +0200

    create login page

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import Emplyees from './pages/Employees'
 import Assets from './pages/Assets'
 import Layout from './Layout';
 import Home from './pages/Home';
+import LogInPage from './pages/LogInPage';
 
 
 function App() {
   return (
     <Routes>
+      <Route path='/login' element={<LogInPage />} />
       <Route path='/' element={<Layout />}>
         <Route path='/employees' element={<Emplyees />} />
         <Route path='/assets' element={<Assets />} />
---
+++ client/src/pages/LogInPage.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button'
+import Alert from 'react-bootstrap/Alert';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleExclamation, faFrog } from '@fortawesome/free-solid-svg-icons'
+
+export default function LogInPage() {
+    return (
+        <Container style={{ maxWidth: '400px', margin: '0 auto' }} className="mt-5">
+            <h1>
+                <FontAwesomeIcon icon={faFrog} bounce style={{ color: "#0b3805", }} /> Ultimate HD
+            </h1>
+            <Alert variant={'danger'} className='d-nsne' style={{ visibility: 'hiddsen' }}><FontAwesomeIcon icon={faCircleExclamation} />This is jfghjfghjfghjfghjfghjgfhjfghjfghjfghja danger
+                alert—check it out!</Alert>
+            <Form>
+                <Form.Group className="mb-3" controlId="formGroupEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formGroupPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Button variant="success" className='ps-4 pe-4' as={Link} to="/">Login</Button>
+            </Form>
+        </Container>
+    )
+}
