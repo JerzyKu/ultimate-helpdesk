7bfd1e157dc08234e445464dcc5e31c09f39b817
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-06-27 17:00:14 +0200

    remove old navbar

--- client/src/components/NavbarComponent.js
+++ client/src/components/NavbarComponent.js
@@ -10,29 +10,6 @@ import { faFrog } from '@fortawesome/free-solid-svg-icons'
 export default function NavbarComponent() {
     return (
         <>
-            {/* <nav className="navbar navbar-expand-lg bg-primary-subtle">
-                <div className="container-fluid">
-                    <span className="navbar-brand mb-0 h1">Ultimate HelpDesk</span>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <Link className="nav-link active" to={'/'}>Home</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to={'/assets'}>Assets</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to={'/employees'}>Employees</Link>
-                            </li>
-
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-            <br /> */}
             <Navbar expand="lg" className="bg-success">
                 <Container>
                     <Navbar.Brand as={Link} to="/"><FontAwesomeIcon icon={faFrog} bounce style={{ color: "#0b3805", }} /> Ultimate HelpDesk</Navbar.Brand>
