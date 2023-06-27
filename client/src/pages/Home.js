608dcc1198c4849a815b362736e1789cf47ff4dc
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-06-27 17:00:36 +0200

    add link to login page, temporery

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+      Home
+      <hr/>
+      <Link to={'/login'} >Login</Link>
+    </>
   )
 }
