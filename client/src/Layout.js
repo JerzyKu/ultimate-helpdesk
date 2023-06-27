6f23584645f7f2f8c82cc3780d93730ca900518e
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-06-27 16:59:48 +0200

    fix layout with container component

--- client/src/Layout.js
+++ client/src/Layout.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from './components/NavbarComponent'
 import { Outlet } from 'react-router-dom'
+import Container from 'react-bootstrap/Container'
 
 export default function Layout() {
     return (
         <>
             <Navbar />
-            <Outlet />
+            <Container>
+                <Outlet />
+            </Container>
         </>
     )
 }
