682eb87d52b65a9f7cdfbabdcb14f2c97d6c87f2
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-06-29 22:30:44 +0200

    rebuldin new employee form

--- client/src/pages/NewEmployee.js
+++ client/src/pages/NewEmployee.js
@@ -1,7 +1,24 @@
 import React from 'react'
 
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 export default function NewEmployee() {
   return (
-    <div>NewEmployee</div>
+    <Form>
+      <Form.Group className="mb-3">
+        <Form.Label>Firstname</Form.Label>
+        <Form.Control type="text" placeholder="Enter email" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Lastname</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   )
 }
