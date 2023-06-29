5a184406da47b2d32a3039d98062006d990f555b
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-06-29 17:15:18 +0200

    create custom links

--- client/src/components/NavbarComponent.js
+++ client/src/components/NavbarComponent.js
@@ -1,46 +1,42 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useMatch, useResolvedPath } from 'react-router-dom'
+
+//bootstrap
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
+
+//font awsome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFrog } from '@fortawesome/free-solid-svg-icons'
+import { faCircleUser, faFrog } from '@fortawesome/free-solid-svg-icons'
 
 export default function NavbarComponent() {
+    console.log(window.location.pathname);
     return (
         <>
-            <Navbar expand="lg" className="bg-success mb-3">
+            <Navbar expand="lg" className="mb-3" style={{ backgroundColor: '#006A4E', color: 'white' }}>
                 <Container>
                     <Navbar.Brand as={Link} to="/"><FontAwesomeIcon icon={faFrog} bounce style={{ color: "#0b3805", }} /> Ultimate HelpDesk</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Toggle />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link as={Link} to="/" >Home</Nav.Link>
-                            <Nav.Link as={Link} to="/assets" active>Assets</Nav.Link>
-                            <Nav.Link as={Link} to="/employees" >Employees</Nav.Link>
-                            <Nav.Link as={Link} to="/longue" disabled>Longue</Nav.Link>
-                            {/* <Nav.Link href="/assets">Assets</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">
-                                    Another action
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
-                            </NavDropdown> */}
+                            <CustomLink to="/" >Home</CustomLink>
+                            <CustomLink to="/assets" active>Assets</CustomLink>
+                            <CustomLink to="/employees" >Employees</CustomLink>
+                            <CustomLink to="/longue" >Longue</CustomLink>
+                            <CustomLink to="/account" ><FontAwesomeIcon icon={faCircleUser} /> Username</CustomLink>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-
-
         </>
-
     )
 }
 
+function CustomLink({ to, children, ...props }) {
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+    return (
+        <Nav.Link as={Link} to={to} {...props} active={isActive}>{children}</Nav.Link>
+    )
+}
