399fe6384e8753172596a3d8555f6d92a2abeb94
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-07-05 15:05:43 +0200

    remove link

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
     <>
       Home
-      <hr/>
-      <Link to={'/login'} >Login</Link>
     </>
   )
 }
