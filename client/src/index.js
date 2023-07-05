44bc585cf3b0d894bab3f14dc88b6416317213e5
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-07-05 15:04:09 +0200

    create and inport styles

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,10 @@ import App from './App';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+
+import './style.css'
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
