e8a0ee79718ad0ad5dfd30d4273f013367173c18
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-07-06 10:39:41 +0200

    create register form

---
+++ client/src/pages/Register.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useRef, useState } from "react";
+import axios from "../api/axios";
+
+//react bootstrap
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
+
+export default function Register() {
+  const usernameRef = useRef();
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState('')
+
+  useEffect(() => {
+    usernameRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    // usernameRef.current.focus();
+    setErrMsg('')
+    // setPwd('')
+  }, [user,pwd]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+        const response = await axios.post('/register', {user, pwd})
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+        setErrMsg(JSON.stringify(error.message))
+    }
+  };
+
+  return (
+    <>
+      <h2>Create New User</h2>
+      {errMsg && <Alert variant='danger'>{errMsg}</Alert>}
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label>username: </Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Username"
+            ref={usernameRef}
+            value={user}
+            onChange={(e) => setUser(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-4">
+          <Form.Label>password: </Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="password"
+            value={pwd}
+            onChange={(e) => setPwd(e.target.value)}
+          />
+        </Form.Group>
+        <Button variant="success" className="ps-4 pe-4" type="submit">
+          Create
+        </Button>
+      </Form>
+    </>
+  );
+}
