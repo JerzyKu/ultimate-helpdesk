fea5e6f1ee381c6bbd10661e2a76b208f7e886ae
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-07-07 10:16:10 +0200

    change username field to unique

--- server/model/User.js
+++ server/model/User.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     username: {
         type: String,
-        require: true
+        require: true,
+        unique: true
     },
     roles: {
         User: {
