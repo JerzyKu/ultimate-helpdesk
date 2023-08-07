dfed675a30ab07c6a89d568f7d430ebcea6a2570
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-08-07 22:08:59 +0200

    add back button

--- client/src/features/assets/SingleAssetPage.js
+++ client/src/features/assets/SingleAssetPage.js
@@ -3,13 +3,15 @@ import { useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
 import { selectAssetByID } from './assetsSlice';
 import AssetOwner from './AssetOwner';
+import Button from 'react-bootstrap/Button';
 
 export default function SingleAssetPage() {
-    let { id } = useParams();
+    const  { id } = useParams();
     const assetData = useSelector(state => selectAssetByID(state, id))
 
     const asset = < div>
         {/* {JSON.stringify(assetData)} <br /> */}
+        <Button as={Link} to='/assets'> {'< Assets'}</Button> <br />
         Name: {assetData.name} <br />
         Inventory symbol: {assetData.invSymbol}<br />
         createdAt: {assetData.createdAt}<br />
