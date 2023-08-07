9cca0903dea0c576066da1fe16c5d54b464af01c
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-08-07 22:06:53 +0200

    commenting consoleLogs

--- client/src/features/assets/AssetOwner.js
+++ client/src/features/assets/AssetOwner.js
@@ -2,9 +2,9 @@ import { useSelector } from "react-redux";
 import { selectAllUsers } from "../users/usersSlice";
 
 export default function AssetOwner({userID}) {
-  console.log(userID);
+  // console.log(userID);
   const users = useSelector(selectAllUsers);
   const owner = users.find((user) => user._id === userID);
-  console.log('owner', owner);
+  // console.log('owner', owner);
   return <>{owner ? `Issued to ${owner.username}` : "ready to issue"}</>;
 }
