c6d5cf47d691e5c3f2556abf4758755756b03994
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-08-07 22:08:33 +0200

    finish creating form

--- client/src/features/assets/EditAssetForm.js
+++ client/src/features/assets/EditAssetForm.js
@@ -1,51 +1,82 @@
 import { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { selectAssetByID } from "./assetsSlice";
+
+import { selectAssetByID, updateAsset } from "./assetsSlice";
 import { selectAllUsers } from "../users/usersSlice";
 
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button'
 
-
 export default function EditAssetForm() {
-    const dispatch = useDispatch()
-
     const { id } = useParams()
+    const navigate = useNavigate()
 
     const usersList = useSelector(selectAllUsers)
-    
     const asset = useSelector(state => selectAssetByID(state, id))
+
     const [assetName, setAssetName] = useState(asset.name)
     const [assetInvSymbol, setAssetInvSymbol] = useState(asset.invSymbol)
+    const [assetOwner, setAssetOwner] = useState(asset.ownerID)
+    const [requsestStatus, setRequestStatus] = useState('idle')
+
+    const dispatch = useDispatch()
+
+
+    const canSave = [assetName, assetInvSymbol, assetOwner].every(Boolean) && requsestStatus === 'idle'
+
+    const onSavePostClicked = () => {
+        if (canSave) {
+            try {
+                setRequestStatus('pending')
+                dispatch(updateAsset({
+                    id: asset._id,
+                    name: assetName,
+                    invSymbol: assetInvSymbol,
+                    ownerID: assetOwner
+                })).unwrap()
+                navigate(`/assets/${id}`)
+            }
+            catch (err) {
+                console.log('Filed to update asset', err);
+            } finally {
+                setRequestStatus('idle')
+            }
+        }
+    }
 
     const usersOptions = usersList.map((user) => (
         <option key={user._id} value={user._id}>
-          {user.username}
+            {user.username}
         </option>
-      ));
+    ));
 
     return (<>
         {/* {JSON.stringify(asset)} */}
         <Form>
-            <Form.Group className="mb-3" controlId="formGroupEmail">
+            <Form.Group className="mb-3">
                 <Form.Label>Name :</Form.Label>
-                <Form.Control type="text" value={assetName}/>
+                <Form.Control type="text" value={assetName} onChange={e => setAssetName(e.target.value)} />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formGroupEmail">
+            <Form.Group className="mb-3" >
                 <Form.Label>Inventory Symbol:</Form.Label>
-                <Form.Control type="text" value={assetInvSymbol}/>
+                <Form.Control type="text" value={assetInvSymbol} onChange={e => setAssetInvSymbol(e.target.value)} />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formGroupEmail">
+            <Form.Group className="mb-3">
                 <Form.Label>Ownerl:</Form.Label>
-                <Form.Select>
-                    <option > test </option>
-                    <option > test 1</option>
-                    <option > test 2</option>
+                <Form.Select
+                    defaultValue={assetOwner}
+                    onChange={e => setAssetOwner(e.target.value)}
+                >
                     {usersOptions}
                 </Form.Select>
             </Form.Group>
-            <Button variant="success" className='ps-4 pe-4'>Update</Button>
+            <Button
+                variant="success"
+                className='ps-4 pe-4'
+                onClick={onSavePostClicked}
+                disabled={!canSave}
+            >Update</Button>
         </Form>
     </>);
 }
