334d0cdd2a2ead37adca9964fbe035d6bed56769
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-08-18 00:21:05 +0200

    idk idk idk

--- client/src/features/assets/assetsSlice.js
+++ client/src/features/assets/assetsSlice.js
@@ -1,48 +1,65 @@
-import {
-  createSelector,
-  createEntityAdapter,
-} from "@reduxjs/toolkit";
+import { createSelector, createEntityAdapter } from "@reduxjs/toolkit";
 import { apiSlice } from "../api/apiSlice";
 
 const assetsAdapter = createEntityAdapter({
   selectId: (entity) => entity._id,
-})
+});
 
-const initialState = assetsAdapter.getInitialState()
+const initialState = assetsAdapter.getInitialState();
 
 export const extendetApiSlice = apiSlice.injectEndpoints({
-  endpoints: builder => ({
+  endpoints: (builder) => ({
     getAssets: builder.query({
-      query: () => '/assets',
-      transformResponse: responseData => assetsAdapter.setAll(initialState, responseData),
+      query: () => "/assets",
+      transformResponse: (responseData) =>
+        assetsAdapter.setAll(initialState, responseData),
 
       providesTags: (result, error, arg) => [
-        { type: 'Asset', id: 'LIST'},
-        ...result.ids.map(id => ({ type: 'Asset', id}))
+        { type: "Asset", id: "LIST" },
+        ...result.ids.map((id) => ({ type: "Asset", id })),
+      ],
+    }),
+    getAssetsByOwnerId: builder.query({
+      query: (id) => `/posts/?userId=${id}`,
+      transformResponse: (responseData) => {
+        return assetsAdapter.setAll(initialState, responseData);
+      },
+      providesTags: (result, error, arg) => {
+        console.log(result);
+        return [...result.ids.map((id) => ({ type: "Asset", id }))];
+      },
+    }),
+    addNewAsset: builder.mutation({
+      query: initailAsset => ({
+        url: `/assets`,
+        method: 'POST',
+        body: initailAsset
+      }), 
+      invalidatesTags: [
+        { type: 'Asset', id: 'LIST'}
       ]
     })
-  })
-})
+  }),
+});
 
-export const {
-  useGetAssetsQuery,
-} = extendetApiSlice
-
-
+export const { useGetAssetsQuery, useGetAssetsByOwnerIdQuery } =
+  extendetApiSlice;
 
 //returns the query result object
-export const selectAsstsResult = extendetApiSlice.endpoints.getAssets.select()
+export const selectAsstsResult = extendetApiSlice.endpoints.getAssets.select();
 
 // creates memoized selector
 const selectAssetData = createSelector(
   selectAsstsResult,
-  assetResult => assetResult.data
-)
+  (assetResult) => assetResult.data
+);
 
-//getSelectors creates these selectors and we rename them with aliases using destructuring 
+//getSelectors creates these selectors and we rename them with aliases using destructuring
 export const {
   selectAll: selectAllAssets,
   selectById: selectAssetById,
-  selectIds: selectAssetsIds
-  // pass in selector that returns 
-} = assetsAdapter.getSelectors(state => selectAssetData(state) ?? initialState)
+  selectIds: selectAssetsIds,
+  // pass in selector that returns
+} = assetsAdapter.getSelectors(
+  (state) => selectAssetData(state) ?? initialState
+);
