f648e8d60c338da8f98bd99d89435c601727a49d
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-09-14 22:34:24 +0200

    update row

--- client/src/features/users/UsersRow.js
+++ client/src/features/users/UsersRow.js
@@ -1,11 +1,19 @@
 import { useSelector } from "react-redux";
 import { selectUserById } from "./usersApiSlice";
+import { useNavigate } from "react-router-dom";
+
 
 export default function UsersRow({ userId }) {
   const user = useSelector((state) => selectUserById(state, userId));
+  const navigate = useNavigate();
+  
+
+  const onClickRow = () => {
+    navigate(`/users/${userId}`)
+  }
 
   return (
-    <tr>
+    <tr onClick={onClickRow}>
       <td>{user.username}</td>
       <td>{JSON.stringify(user.roles)}</td>
     </tr>
