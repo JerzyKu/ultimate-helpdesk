f7679a9dbd187f4435bbaa7376f013fabf269039
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-09-17 23:13:24 +0200

    add click event to asset row

--- client/src/features/assets/AssetsRow.js
+++ client/src/features/assets/AssetsRow.js
@@ -1,11 +1,13 @@
 import { useSelector } from "react-redux";
 import { selectAssetsById } from "./assetsApiSlice";
+import { useNavigate } from "react-router-dom";
 
 export default function AssetsRow({ assetId }) {
   const asset = useSelector((state) => selectAssetsById(state, assetId));
+  const navigate = useNavigate()
 
   return (
-    <tr className="cursor-pointer">
+    <tr className="cursor-pointer" onClick={() => navigate(`${asset.id}`)}>
       <td>{asset.name}</td>
       <td>{asset.invSymbol}</td>
       <td>{asset.ownerID}</td>
