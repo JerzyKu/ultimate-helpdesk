2783c32bce0e696941b6561734e5d52a7f451d0a
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-09-17 23:13:41 +0200

    add assets to prefetch

--- client/src/features/auth/Prefetch.js
+++ client/src/features/auth/Prefetch.js
@@ -2,15 +2,18 @@ import { useEffect } from "react";
 import { store } from "../../app/store";
 import { usersApiSlice } from "../users/usersApiSlice";
 import { Outlet } from "react-router-dom";
+import { asstesApiSlice } from "../assets/assetsApiSlice";
 
 export default function Prefetch() {
   useEffect(() => {
     console.log("subscribing");
     const users = store.dispatch(usersApiSlice.endpoints.getUsers.initiate());
+    const assets = store.dispatch(asstesApiSlice.endpoints.getAssets.initiate())
 
     return () => {
       console.log("ubsubscribing");
       users.unsubscribe();
+      assets.unsubscribe();
     };
   }, []);
 
