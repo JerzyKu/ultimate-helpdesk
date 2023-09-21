fcd20cd679f2f3e33ec7d6a0081979339dfdcce8
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-09-21 16:52:13 +0200

    add fields to schema

--- server/model/User.js
+++ server/model/User.js
@@ -1,29 +1,43 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+    firstName: { 
+        type: String 
+    },
+    lastName: { 
+        type: String 
+    },
+    email: { 
+        type: String 
+    },
+    jobTitle: { 
+        type: String 
+    },
     username: {
-        type: String,
-        require: true,
-        unique: true
+      type: String,
+      require: true,
+      unique: true,
     },
     roles: {
-        User: {
-            type: Number,
-            default: 2001
-        },
-        Editor: Number,
-        Admin: Number
+      User: {
+        type: Number,
+        default: 2001,
+      },
+      Editor: Number,
+      Admin: Number,
     },
     password: {
-        type: String,
-        // require: true
+      type: String,
+      // require: true
     },
     refreshToken: {
-        type: String
-    }
-}, {
-    timestamps: true
-})
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema);
