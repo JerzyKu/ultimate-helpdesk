9d3fe6a76f33417fa73fe911dec2d1a062697963
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-09-26 11:46:42 +0200

    kkkkkkkk

--- client/src/features/api/apiSlice.js
+++ client/src/features/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localost:3500'}),
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500'}),
     tagTypes: ['Asset'],
     endpoints: builder => ({})
 })
