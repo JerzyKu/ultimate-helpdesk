2cc36af98afcc7d8e4bd5ec5b97b4ca05c9b7d35
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-10-22 22:51:25 +0200

    move home

--- client/src/features/auth/Home.js
+++ client/src/features/auth/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import useAuth from '../../hooks/useAuth'
 // import { Link } from 'react-router-dom'
 
 export default function Home() {
+  const { username } = useAuth()
   return (
     <>
-      Home
+      Hello {username}!
     </>
   )
 }
