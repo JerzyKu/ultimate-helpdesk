eee011d3c133059cb58f6351529c679b1975a02e
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-10-24 17:11:49 +0200

    add page to cors

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'https://www.dandrepairshop.com',
-    'https://dandrepairshop.com'
+    'https://dandrepairshop.com',
+    'https://irish-fuel-elementary-currency.trycloudflare.com'
 ]
 
 module.exports = allowedOrigins
