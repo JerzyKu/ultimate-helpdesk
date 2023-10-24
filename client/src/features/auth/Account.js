e74fd0efd7478314f7d00258264c9a170a38ea5e
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-10-24 23:41:46 +0200

    create skeleton of change password

--- client/src/features/auth/Account.js
+++ client/src/features/auth/Account.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
 
 export default function Account() {
   return (
-    <div>Account</div>
-  )
+    <>
+      <h3>Account</h3>
+
+      <FloatingLabel
+        controlId="floatingInput"
+        label="Old Password"
+        className="mb-1"
+      >
+        <Form.Control type="text" placeholder="" />
+      </FloatingLabel>
+
+      <FloatingLabel
+        controlId="floatingInput"
+        label="New Password"
+        className="mb-1"
+      >
+        <Form.Control type="text" placeholder="" />
+      </FloatingLabel>
+
+      <FloatingLabel
+        controlId="floatingInput"
+        label="Repin New Password"
+        className="mb-1"
+      >
+        <Form.Control type="text" placeholder="" />
+      </FloatingLabel>
+
+      <Button variant="primary">Change Password</Button>
+    </>
+  );
 }
