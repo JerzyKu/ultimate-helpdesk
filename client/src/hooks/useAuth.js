e74b8bea10dee56dae504d20b7fecb33e8167613
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-10-27 23:10:38 +0200

    add isAdmin and IsHelpDesk fields

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -4,17 +4,20 @@ import jwtDecode from "jwt-decode";
 
 export default function useAuth() {
   const token = useSelector(selectCurrentToken);
+
+  let isAdmin = false 
+  let isHelpDesk = false
+
  
 
   if (token) {
     const decoded = jwtDecode(token);
     const { username, roles, firstName } = decoded.UserInfo;
-    console.log(decoded.UserInfo);
 
-   
+    isAdmin = roles.includes('Admin')
 
-    return { username, roles, firstName };
+    return { username, roles, firstName, isAdmin, isHelpDesk };
   }
 
-  return { username: "", roles:[], firstName: '' };
+  return { username: "", roles:[], firstName: '', isAdmin, isHelpDesk };
 }
