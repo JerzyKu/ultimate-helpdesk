7359a3da8f4ee5016bc541a47ee5965f3dbcb6d0
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-10-29 07:18:47 +0100

    refactor uer row

--- client/src/features/users/UsersRow.js
+++ client/src/features/users/UsersRow.js
@@ -1,23 +1,32 @@
 import { useSelector } from "react-redux";
 import { selectUserById } from "./usersApiSlice";
 import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import Button from "react-bootstrap/Button";
 
 export default function UsersRow({ userId }) {
   const user = useSelector((state) => selectUserById(state, userId));
   const navigate = useNavigate();
 
-  const onClickRow = () => {
-    navigate(`/users/${userId}`);
-  };
-
   return (
-    <tr onClick={onClickRow} className="cursor-pointer">
-      <td>{user.firstName}</td>
-      <td>{user.lastName}</td>
-      <td>{user.jobTitle}</td>
+    <tr>
+      <td>
+        {user.firstName} {user.lastName}
+      </td>
       <td>{user.username}</td>
+      <td>{user.jobTitle}</td>
       <td>{user.active ? "V" : "X"}</td>
-      <td>{JSON.stringify(user.roles)}</td>
+      {/* <td>{JSON.stringify(user.roles)}</td> */}
+      <td>
+        <Button variant="primary">
+          <FontAwesomeIcon
+            className="cursor-pointer"
+            icon={faPenToSquare}
+            onClick={() => navigate(`/users/${userId}`)}
+          />
+        </Button>
+      </td>
     </tr>
   );
 }
