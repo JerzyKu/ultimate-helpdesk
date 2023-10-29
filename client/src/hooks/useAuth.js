1222e0e80dd88bdd15ccf599a931be41f964a2e6
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-10-29 10:17:50 +0100

    add id

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -5,19 +5,24 @@ import jwtDecode from "jwt-decode";
 export default function useAuth() {
   const token = useSelector(selectCurrentToken);
 
-  let isAdmin = false 
-  let isHelpDesk = false
-
- 
+  let isAdmin = false;
+  let isHelpDesk = false;
 
   if (token) {
     const decoded = jwtDecode(token);
-    const { username, roles, firstName } = decoded.UserInfo;
+    const { username, roles, firstName, id } = decoded.UserInfo;
 
-    isAdmin = roles.includes('Admin')
+    isAdmin = roles.includes("Admin");
 
-    return { username, roles, firstName, isAdmin, isHelpDesk };
+    return { username, roles, firstName, isAdmin, isHelpDesk, id };
   }
 
-  return { username: "", roles:[], firstName: '', isAdmin, isHelpDesk };
+  return {
+    username: "",
+    roles: [],
+    firstName: "",
+    isAdmin,
+    isHelpDesk,
+    id: "",
+  };
 }
