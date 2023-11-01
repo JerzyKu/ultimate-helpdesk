a4f0db6ec63f8f06d31fb95b0ac09283ff7df1e6
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-11-01 17:26:11 +0100

    add tooltip

--- client/src/features/users/UsersRow.js
+++ client/src/features/users/UsersRow.js
@@ -5,9 +5,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import Button from "react-bootstrap/Button";
 
+import { Tooltip } from "react-tooltip";
+import { useId } from "react";
+
 export default function UsersRow({ userId }) {
   const user = useSelector((state) => selectUserById(state, userId));
   const navigate = useNavigate();
+
+  const buttonId = useId()
 
   return (
     <tr>
@@ -19,13 +24,20 @@ export default function UsersRow({ userId }) {
       <td>{user.active ? "V" : "X"}</td>
       {/* <td>{JSON.stringify(user.roles)}</td> */}
       <td>
-        <Button variant="primary">
+        <Button
+          variant="primary"
+          data-tooltip-id={buttonId}
+          data-tooltip-content="Edit User."
+          data-tooltip-place="bottom"
+          data-tooltip-delay-show={1000}
+        >
           <FontAwesomeIcon
             className="cursor-pointer"
             icon={faPenToSquare}
             onClick={() => navigate(`/users/${userId}`)}
           />
         </Button>
+        <Tooltip id={buttonId} />
       </td>
     </tr>
   );
